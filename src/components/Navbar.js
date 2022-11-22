10d2319f3b17b1582b2bda5046ea37e7071db06c
25braincells <yi.thanyaluck_st@tni.ac.th>
2022-11-22 23:36:57 +0700

    add components

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import { Component } from "react";
+import "./NavbarStyles.css";
+import {MenuItems} from './MenuItems';
+
+class Navbar extends Component{
+    render(){
+        return(
+            <nav className="NavbarItems">
+                <h1 className="nav-logo">Sleepy</h1>
+                <ul className="nav-menu">
+                    {MenuItems.map((item, index)=>{
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href="/"><i className={item.icon}></i>{item.title}</a>
+                            </li>
+                        )
+                    })}
+                    
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
