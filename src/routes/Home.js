a8f91c71c9f195ef09e4d0a182db0ed105960179
25braincells <yi.thanyaluck_st@tni.ac.th>
2022-11-23 15:34:46 +0700

    changes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,9 +12,7 @@ export default function Home (){
             cName = "hero"
             heroImg = "https://images.unsplash.com/photo-1489440543286-a69330151c0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
             title = "'Rescued' is my favourite breed."
-            btnText = "Adopt Here!"
             url="/"
-            btnClass="show"
             />
             <Dog />
             <Adopt />
